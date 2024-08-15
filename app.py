bc95e9150b69e58cda9069fb2669855e61f0c711
NAYAN BHANSALI <nayanbhansali22@gmail.com>
2024-08-15 17:11:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def predict():
                                review=review)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
